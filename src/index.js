98a32c5bcd9f7c31ed9d3cd407e8a1fef48e01e5
alexandre-mace <contact.alexandremace@gmail.com>
2020-02-03 20:16:29 +0100

    Update mobile size fix

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import rootReducer from './reducers'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
