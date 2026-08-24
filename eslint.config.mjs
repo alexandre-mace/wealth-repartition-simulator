90b6dc6777b60dace1064fb7d73ae6377f89e187
Alexandre Macé <helloalexandremace@protonmail.com>
2026-08-24 13:34:21 +0200

    chore: config ESLint flat manquante + bascule pnpm

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "eslint/config";
+import coreWebVitals from "eslint-config-next/core-web-vitals";
+import typescript from "eslint-config-next/typescript";
+
+export default defineConfig([
+  ...coreWebVitals,
+  ...typescript,
+  { ignores: [".next/**", "out/**", "node_modules/**"] },
+]);
